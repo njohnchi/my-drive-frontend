ef9488e9a2c2b156e961e8251154780fac3f6c46
Nnamani John <nnamanijohn123@gmail.com>
2025-01-04 04:51:13 +0100

    setup nuxt ui

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-amber-800 w-full">
+    <h1>Hello</h1>
+    <UButton
+      icon="i-heroicons-pencil-square"
+      size="sm"
+      color="primary"
+      variant="solid"
+      label="Button"
+      :trailing="false"
+    />
   </div>
 </template>
