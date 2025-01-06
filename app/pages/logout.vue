070d389d59611bd4ab1a64adc092e9154a9c04e4
Nnamani John <nnamanijohn123@gmail.com>
2025-01-06 02:46:40 +0100

    add logout route

---
+++ app/pages/logout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { logout } = useSanctumAuth()
+await logout()
+useRouter().replace('/login')
+</script>
+
+<template>
+  <p>logging out...</p>
+</template>
+
+<style scoped>
+
+</style>
