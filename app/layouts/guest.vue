3d32dd97069559486dbff56b8c9f9f0f7839c81e
Nnamani John <nnamanijohn123@gmail.com>
2025-01-16 08:09:04 +0100

    add logo to guest layout

--- app/layouts/guest.vue
+++ app/layouts/guest.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-  <div class="flex justify-center py-12">
+  <div class="flex flex-col items-center py-12">
+    <header>
+      <h1 class="text-3xl uppercase font-bold text-primary mb-3">
+        <ULink to="/">My Drive</ULink>
+      </h1>
+    </header>
     <div class="min-w-96 bg-gray-700 rounded p-8">
-      <slot />
+      <main>
+        <slot />
+      </main>
     </div>
   </div>
 </template>
